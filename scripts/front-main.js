a7c287321cb406d781d99ab2ef21fa822a855643
Malik Morenigbade <malikmoreni@gmail.com>
2016-12-18 18:48:43 +0100

    Update front-main.js

--- scripts/front-main.js
+++ scripts/front-main.js
@@ -35,7 +35,7 @@ require(['jquery-private','typewrite.min'],function($){
 				}
 			});
 			let introtype = new typeWrite(".intro .type", {
-				'texts': ['I am a junior Front-End Developer & a sophomore of the University of Lodz, currently working as a freelancer. I stand on a sweet spot where design & code intersects.'],
+				'texts': ['I\'m a Front-End Developer & a sophomore of the University of Lodz, currently working as a freelancer. I stand on a sweet spot where design & code intersects.'],
 				'loop': false,
 				'cursor': ' ',
 				'reverseDelay': 1000,
